1df593e229ac855c08c6dadcd05702d0cc4b3676
nb341 <narindrab4@gmail.com>
2021-03-15 18:28:38 -0400

    first commit

---
+++ polls/apiviews.py
@@ -0,0 +1,71 @@
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from .models import Poll, Choice
+from rest_framework import viewsets
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer, UserSerializer
+from rest_framework.exceptions import PermissionDenied
+
+# We have seen 4 ways to build API views until now
+# • Pure Django views
+# • APIView subclasses
+# • generics.* subclasses
+# • viewsets.ModelViewSet
+# So which one should you use when? My rule of thumb is,
+# • Use viewsets.ModelViewSet when you are going to allow all or most of CRUD operations on a model.
+# • Use generics.* when you only want to allow some operations on a model
+# • Use APIView when you want to completely customize the behaviour.
+
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    def destroy(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not delete this poll.")
+        return super().destroy(request, *args, **kwargs)
+
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
+        return queryset
+    serializer_class = ChoiceSerializer
+    def post(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not create choice for this poll.")
+        return super().post(request, *args, **kwargs)
+
+
+class CreateVote(APIView):
+    serializer_class = VoteSerializer
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserCreate(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+
+    serializer_class = UserSerializer
+
+class LoginView(APIView):
+    permission_classes = ()
+    def post(self, request,):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
